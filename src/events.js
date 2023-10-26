f977ef260b50b09514da6be50352330c0c2dd6f5
nodejavascript <nodejsjavascript@gmail.com>
2023-10-26 14:18:49 -0400

    ability to publish command to listClients

--- src/events.js
+++ src/events.js
@@ -1,3 +1,5 @@
+const { AEDES_COMMAND_TOPIC } = process.env
+
 export const handleConnection = (aedes, event, client = null) => {
   const logline = returnLogline(event)
 
@@ -6,13 +8,40 @@ export const handleConnection = (aedes, event, client = null) => {
   displayLogline(logline)
 }
 
+const listClients = aedes => {
+  const clients = Object.keys(aedes.clients).map(key => {
+    const client = aedes.clients[key]
+
+    return {
+      client: key,
+      subscriptions: Object.keys(client.subscriptions),
+      remoteAddress: client.conn.remoteAddress
+    }
+  })
+
+  aedes.publish({ topic: `${AEDES_COMMAND_TOPIC}/listClients`, payload: JSON.stringify(clients) })
+}
+
+const handleCommand = (aedes, { topic, payload: payloadBuf }) => {
+  if (!topic) return
+  if (topic.includes('/') < 0) return
+
+  const [auth, cmd] = topic.split('/')
+  if (auth !== AEDES_COMMAND_TOPIC) return
+  if (cmd !== 'cmd') return
+
+  const payload = payloadBuf.toString()
+
+  if (payload === 'listClients') listClients(aedes)
+}
+
 export const handleEvent = (aedes, event, message, args) => {
+  handleCommand(aedes, message)
   const logline = returnLogline(event)
 
   const clientId = args ? args.id : aedes.id
 
   logline.push(`clientId: ${clientId}`)
-
   const { topic, payload } = message
 
   if (payload) logline.push(`payload (${typeof payload}), ${payload.toString()}`)
